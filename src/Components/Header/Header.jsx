550d8a8e43349948626bfa38adb3d3c18167af93
anton amir <amiranton257@gmail.com>
2023-11-23 00:58:49 +0200

    edit header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,22 +21,6 @@ function Header({ setCoordinates }) {
         <Typography variant="h5" className={classes.title}>
           Travel Advisor
         </Typography>
-        <Box display="flex">
-          <Typography variant="h6" className={classes.title}>
-            Explore new places
-          </Typography>
-          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search ..."
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
-            </div>
-          </Autocomplete>
-        </Box>
       </Toolbar>
     </AppBar>
   );
